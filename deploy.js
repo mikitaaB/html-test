aca51616d7d0a9caaa95c9cbc3bdcf003c90b892
Mikita <nikita.492@yandex.by>
2023-04-07 04:38:56 +0300

    Minor changes

--- deploy.js
+++ deploy.js
@@ -1,10 +1,10 @@
 import ghpages from 'gh-pages';
 
 ghpages.publish('./build', {
-    add: true,
-    branch: 'gh-pages',
-    repo: 'git@github.com:mikitaab/html-test.git'
-},
+        add: true,
+        branch: 'gh-pages',
+        repo: 'git@github.com:mikitaab/html-test.git'
+    },
     (err) => {
         if (err) console.error("ERROR: ", err);
         else console.log("Published");
