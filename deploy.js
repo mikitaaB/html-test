47204da9d6eedd7f5d2e6f9e2dcad5c73ec6e580
Mikita <nikita.492@yandex.by>
2023-04-19 16:19:46 +0300

    Spaces to tabs

--- deploy.js
+++ deploy.js
@@ -1,15 +1,15 @@
-import ghpages from 'gh-pages';
+import ghpages from "gh-pages";
 
-ghpages.publish('./build', {
-    add: true,
-    branch: 'gh-pages',
-    repo: 'git@github.com:mikitaab/html-test.git'
+ghpages.publish("./build", {
+	add: true,
+	branch: "gh-pages",
+	repo: "git@github.com:mikitaab/html-test.git"
 },
-    (err) => {
-        if (err) {
-            console.error("ERROR: ", err);
-        } else {
-            console.log("Published");
-        }
-    }
+	(err) => {
+		if (err) {
+			console.error("ERROR: ", err);
+		} else {
+			console.log("Published");
+		}
+	}
 );
