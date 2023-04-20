cbb23a95d76bb5b2155d28e5ef05bedb875bc08e
mikitaaB <mikitaaB@users.noreply.github.com>
2023-04-20 02:26:58 +0000

    deploy: f229f123790528a707dcd8cffb1bbdd450d40e8d

---
+++ app.js
@@ -0,0 +1,45 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+const handleInputChange = (e) => {
+	const target = e.target;
+	if (e.target.type !== "range") {
+		target = rangeSlider;
+	}
+	const rangeSpan = document.getElementById("range-span");
+	rangeSpan.textContent = target.value + "%";
+}
+
+const rangeSlider = document.getElementById("range-slider");
+rangeSlider.addEventListener("input", handleInputChange);
+
+const select = document.querySelector(".select-system");
+const selectTitle = select.querySelector(".select-system__title");
+const selectLabels = select.querySelectorAll(".select-system__label");
+
+select.addEventListener("click", () => {
+	if ("active" === select.getAttribute("data-state")) {
+		select.setAttribute("data-state", "");
+	} else {
+		select.setAttribute("data-state", "active");
+	}
+});
+
+for (let i = 0; i < selectLabels.length; i++) {
+	selectLabels[i].addEventListener("click", e => {
+		selectTitle.textContent = e.target.textContent;
+		select.setAttribute("data-state", "");
+	});
+}
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
